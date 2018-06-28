5f3571ed1e45cdbd5e19f81c23ba4365f1eb01ad
MattVoda <mattvodakov@gmail.com>
2018-06-28 18:07:22 -0400

    Hover Effects via Animation
    Built little animations (none, all) and transitioning between by setting a conditional via script. Files are checking for overlap.
    Next - lower sphere opacity

--- Assets/Scripts/FileResponseToHover.cs
+++ Assets/Scripts/FileResponseToHover.cs
@@ -5,14 +5,15 @@ using Valve.VR.InteractionSystem;
 
 public class FileResponseToHover : MonoBehaviour
 {
-    
-
     private Renderer rend;
+    private Animator anim;
+    private int transitionTriggerBool = Animator.StringToHash("engaged");
 
 
     // Use this for initialization
     void Start() {
         rend = GetComponent<Renderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,13 +25,20 @@ public class FileResponseToHover : MonoBehaviour
         if (hand.currentAttachedObject.tag == "selector") {
             //print("hovering with sphere!");
 
-            rend.material.color = Color.red;
-
+            //rend.material.color = Color.red;
+            //anim.Play(allHash);
+            anim.SetBool(transitionTriggerBool, true);
         }
     }
 
     private void HandHoverUpdate(Hand hand) {
         
         
+    }
+
+    private void OnHandHoverEnd(Hand hand) {
+        if (hand.currentAttachedObject.tag == "selector") {
+            anim.SetBool(transitionTriggerBool, false);
+        }
     }
 }

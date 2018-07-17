90f4e643b2ceb9461f4f8331979c0d81d8271e57
MattVoda <mattvodakov@gmail.com>
2018-07-17 18:59:03 -0400

    icons installed

---
+++ Assets/ImportedAssets/FAMesh/Scripts/BouncyIndicator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncyIndicator : MonoBehaviour {
+
+    public float Amount = .1f;
+
+    float baseScale = 1f;
+    private void Start()
+    {
+        baseScale = transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float s = Mathf.Sin(Time.time);
+        transform.Translate(0f, s * Amount, 0f);
+        transform.localScale = new Vector3(baseScale + s * Amount , baseScale + s * Amount , baseScale + s * Amount );
+        var amt = Mathf.Sin(Time.time) * 10f;
+        transform.rotation = Quaternion.Euler(0f, amt, 0f);
+        //transform.Rotate(Vector3.up, amt);
+
+    }
+}
---
+++ Assets/ImportedAssets/FAMesh/Scripts/DemoFountain.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoFountain : MonoBehaviour {
+
+    public GameObject[] prefabs;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

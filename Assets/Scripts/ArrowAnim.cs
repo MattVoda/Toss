d6a1c53feff0a848906d0db83f9ad401b4b4267c
MattVoda <mattvodakov@gmail.com>
2018-07-17 17:39:17 -0400

    Happy with arrowing

---
+++ Assets/Scripts/ArrowAnim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cogobyte.ProceduralIndicators;
+
+//Demo for made of shapes arrow mode in FeaturesShowCase
+//Oscilates the path of arrow along x axis 
+public class ArrowAnim : MonoBehaviour
+{
+    public ArrowObject arrowObject;
+    float fi = 0;
+
+    public Transform startPoint;
+    public Transform endPoint;
+
+    void Update() {
+        arrowObject.arrowPath.startPoint = startPoint.position;
+        arrowObject.arrowPath.endPoint = new Vector3(endPoint.position.x, endPoint.position.y, endPoint.position.z * 10 * Mathf.Abs(Mathf.Sin(fi)));
+        arrowObject.updateArrowMesh();
+        fi += 1f * Time.deltaTime;
+    }
+}

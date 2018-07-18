24fe3056c92770e5bca9575ec5174a420e96eb6a
MattVoda <mattvodakov@gmail.com>
2018-07-18 16:25:15 -0400

    Added more enviro cubes for recording

--- Assets/Scripts/ColorButton.cs
+++ Assets/Scripts/ColorButton.cs
@@ -7,13 +7,19 @@ public class ColorButton : MonoBehaviour {
 
     public Material material;
     public GameObject enviroCube;
+    public GameObject enviroCube1;
+    public GameObject enviroCube2;
     private MeshRenderer enviroRenderer;
+    private MeshRenderer enviroRenderer1;
+    private MeshRenderer enviroRenderer2;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         GetComponent<MeshRenderer>().material = material;
         enviroRenderer = enviroCube.GetComponent<MeshRenderer>();
-	}
+        enviroRenderer1 = enviroCube1.GetComponent<MeshRenderer>();
+        enviroRenderer2 = enviroCube2.GetComponent<MeshRenderer>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -22,5 +28,7 @@ public class ColorButton : MonoBehaviour {
 
     private void OnHandHoverBegin(Hand hand) {
         enviroRenderer.material = material;
+        enviroRenderer1.material = material;
+        enviroRenderer2.material = material;
     }
 }

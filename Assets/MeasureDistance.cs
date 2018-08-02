e5180a98c72c141aae79dcae8383e64aa2f9c760
MattVoda <mattvodakov@gmail.com>
2018-08-02 14:17:34 -0400

    woops working on buttons

---
+++ Assets/MeasureDistance.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeasureDistance : MonoBehaviour {
+
+
+    public GameObject back;
+    public GameObject buttonFace;
+
+    private TextMesh output;
+    private float distance;
+
+	// Use this for initialization
+	void Start () {
+        output = GetComponent<TextMesh>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        distance = Vector3.Distance(back.transform.position, buttonFace.transform.position);
+        output.text = distance.ToString();
+	}
+}
---
+++ Assets/SnapToPosition.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapToPosition : MonoBehaviour {
+    
+    public Transform buttonFace;
+    public Transform back;
+    public float minZ;
+    public float maxZ;
+    public float[] anchorPoints;
+    public float snapDistance = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+        buttonFace = gameObject.transform;
+        minZ = transform.position.y;
+        maxZ = back.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SnapPosition() {
+        //Cycle through each predefined anchor point
+        for (int i = 0; i < anchorPoints.Length; i++) {
+            //If lever is within "snapping distance" of that anchor point
+            if (Mathf.Abs(buttonFace.localPosition.z - anchorPoints[i]) < snapDistance) {
+                //Get current lever position and update z pos to anchor point
+                Vector3 position = buttonFace.transform.localPosition;
+                position.z = anchorPoints[i];
+                buttonFace.transform.localPosition = position;
+
+                //Break so it stops checking other anchor points
+                break;
+            }
+        }
+    }
+}

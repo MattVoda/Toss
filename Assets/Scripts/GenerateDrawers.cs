7485e988de0be5d231ad6d3fc082024661151fec
MattVoda <mattvodakov@gmail.com>
2018-08-16 15:06:45 -0400

    Drawer handle working. Added VRTK examples.

---
+++ Assets/Scripts/GenerateDrawers.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateDrawers : MonoBehaviour {
+
+    public int drawerCount = 5;
+    public float spaceAdjuster = 0.7f;
+    public GameObject contents;
+    //public GameObject depthMasker;
+    public GameObject drawerPrefab;
+
+    private float drawerScale;
+    private Vector3 contentsPos;
+
+	// Use this for initialization
+	void Start () {
+
+        contentsPos = contents.transform.position;
+        drawerScale = drawerPrefab.transform.localScale.x;
+
+        for (int x=0; x<drawerCount; x++) {
+            //Vector3 nextDrawerPos = contentsPos + new Vector3(0, 0, x - x*(1-drawerScale)); //perfectly touching, handles all prefab sizes
+            Vector3 nextDrawerPos = contentsPos + new Vector3(0, 0, x - x * spaceAdjuster); //allows gaps
+            Instantiate<GameObject>(drawerPrefab, nextDrawerPos, contents.transform.rotation, contents.transform);
+        }
+
+        //depthMasker.GetComponent<ResizeToFitDrawers>().ResizeMask(); //tell the mask object what dimensions are needed
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ResizeToFitDrawers.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResizeToFitDrawers : MonoBehaviour {
+
+    public GameObject contents;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ResizeMask() {
+        Vector3 sizeCalculated = contents.GetComponent<Renderer>().bounds.size;
+        transform.localScale = sizeCalculated;
+        print("processed");
+    }
+}

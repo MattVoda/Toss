c8c22fc91e70ed740b7914bfcd96f2c7b2a9d310
MattVoda <mattvodakov@gmail.com>
2018-08-19 23:59:20 -0400

    Reticles made! Big hour haha

---
+++ Assets/Scripts/RandomMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMovement : MonoBehaviour {
+
+    private Transform stuff; // Needs rigidbody attached with a collider
+    private Rigidbody rb;
+    Vector3 vel; // Holds the random velocity
+    float switchDirection = 3f;
+    float curTime = 0f;
+
+	// Use this for initialization
+	void Start() {
+        stuff = gameObject.transform;
+        rb = stuff.GetComponent<Rigidbody>();
+        SetVel();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (curTime < switchDirection) {
+            curTime += 1 * Time.deltaTime;
+        } else {
+            SetVel();
+            if (Random.value > .5) {
+                switchDirection += Random.value;
+            } else {
+                switchDirection -= Random.value;
+            }
+                if (switchDirection < 1) {
+                    switchDirection = 1 + Random.value;
+                }
+                curTime = 0;
+            }
+        }
+
+    void FixedUpdate() {
+        rb.velocity = vel;
+    }
+
+    void SetVel() {
+        //print(Random.value);
+        if (Random.value > .5) {
+            vel.x = 10 * .2f * Random.value;
+        } else {
+            vel.x = -10 *.2f * Random.value;
+        }
+        //if (Random.value > .5) {
+        //    vel.y = 10 * 1 * Random.value;
+        //} else {
+        //    vel.y = -10 * 1 * Random.value;
+        //}
+    }
+}
---
+++ Assets/Scripts/Reticle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reticle : MonoBehaviour {
+
+    public Camera hmdFacing;
+    public GameObject target;
+    public float reticleDistanceFromTarget = 0.5f;
+    public float reticleScale = 1f;
+
+    private Ray reticleRay;
+    private Vector3 adjustedTarget;
+    private float rayDistance;
+    private Vector3 reticlePoint;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //positions reticle in center of vision
+        //transform.position = hmdFacing.transform.position + hmdFacing.transform.rotation * Vector3.forward * 2.0f; 
+
+        rayDistance = Vector3.Distance(hmdFacing.transform.position, target.transform.position);
+        //the ray shoots at a DIRECTION, not a point. Need to subtract out HMD position to be left with relative direction.
+        adjustedTarget = target.transform.position - hmdFacing.transform.position;
+        reticleRay = new Ray(hmdFacing.transform.position, adjustedTarget);
+        //Physics.Raycast(reticleRay, rayDistance);
+        reticlePoint = reticleRay.GetPoint(rayDistance - reticleDistanceFromTarget);
+
+        transform.position = reticlePoint;
+        transform.LookAt(hmdFacing.transform.position);
+        transform.Rotate(0f, 180f, 0f);
+        transform.localScale = new Vector3(reticleScale, reticleScale, reticleScale);
+    }
+}
